9534eba778bf7c868d7d9c501a4b9161a1d39d4f
szangyi <alexandra.mszrs@gmail.com>
2021-02-23 23:21:57 +0100

    sort still doesnt work

--- web/script.js
+++ web/script.js
@@ -2,8 +2,6 @@
 
 window.addEventListener("DOMContentLoaded", start);
 
-const allStudents = [];
-
 const Student = {
   firstName: "",
   lastName: "",
@@ -13,6 +11,8 @@ const Student = {
   house: "",
 };
 
+const allStudents = [];
+
 //set global variables for sort and filter
 const settings = {
   filterBy: "all",
@@ -21,10 +21,9 @@ const settings = {
 };
 
 function start() {
-  console.log("ready");
+  // console.log("ready");
   // TODO: Add event-listeners to filter and sort buttons
   registerButtons();
-
   loadJSON();
 }
 
@@ -33,9 +32,7 @@ function registerButtons() {
     .querySelectorAll("[data-action='filter']")
     .forEach((button) => button.addEventListener("click", selectFilter));
 
-  document
-    .querySelectorAll("[data-action='sort']")
-    .forEach((button) => button.addEventListener("click", selectSort));
+  document.querySelectorAll("[data-action='sort']").forEach((button) => button.addEventListener("click", selectSort));
 }
 
 function loadJSON() {
@@ -47,118 +44,69 @@ function loadJSON() {
 }
 
 function prepareObjects(jsonData) {
-  console.log("jsonprepareloads");
-  jsonData.forEach((student) => {
-    // we use "Student" prototype to create "newStudent"
-    const newStudent = Object.create(Student);
+  // console.log("jsonprepareloads");
+  jsonData.forEach((jsonObject) => {
+    // create a new object with clean data
+    const student = Object.create(Student);
 
-    const fullNameTrim = student.fullname.trim();
+    const fullNameTrim = jsonObject.fullname.trim();
     const firstSpace = fullNameTrim.indexOf(" ");
     const lastSpace = fullNameTrim.lastIndexOf(" ");
 
     //FIRST NAME
     let firstName = fullNameTrim.substring(0, firstSpace);
     let firstNameTrim = firstName.trim();
-    let firstNameFinal =
-      firstNameTrim.charAt(0).toUpperCase() +
-      firstNameTrim.substring(1).toLowerCase();
+    let firstNameFinal = firstNameTrim.charAt(0).toUpperCase() + firstNameTrim.substring(1).toLowerCase();
 
     //MIDDLE NAME
     let middleName = fullNameTrim.substring(firstSpace + 1, lastSpace);
     let middleNameTrim = middleName.trim();
-    let middleNameFinal =
-      middleNameTrim.charAt(0).toUpperCase() +
-      middleNameTrim.substring(1).toLowerCase();
+    let middleNameFinal = middleNameTrim.charAt(0).toUpperCase() + middleNameTrim.substring(1).toLowerCase();
 
     //LAST NAME
     let lastName = fullNameTrim.substring(lastSpace);
     let lastNameTrim = lastName.trim();
-    let lastNameFinal =
-      lastNameTrim.charAt(0).toUpperCase() +
-      lastNameTrim.substring(1).toLowerCase();
+    let lastNameFinal = lastNameTrim.charAt(0).toUpperCase() + lastNameTrim.substring(1).toLowerCase();
+
+    //HOUSE
+    const house = jsonObject.house.trim();
+    const houseFinal = house[0].toUpperCase() + house.substring(1).toLowerCase();
 
     //FULLNAME
-    function showFullname(firstName, middleName, lastName) {
-      // console.log("naaaaa")
-      if (middleName) {
-        const fullNameFinal = `${firstNameFinal} ${middleNameFinal} ${lastNameFinal}`;
-        console.log(fullNameFinal);
-      } else {
-        const fullNameFinal = `${firstNameFinal} ${lastNameFinal}`;
-        // console.log(fullNameFinal);
-      }
-    }
+    // function showFullname(firstName, middleName, lastName) {
+    //   // console.log("naaaaa")
+    //   if (middleName) {
+    //     const fullNameFinal = `${firstNameFinal} ${middleNameFinal} ${lastNameFinal}`;
+    //     console.log(fullNameFinal);
+    //   } else {
+    //     const fullNameFinal = `${firstNameFinal} ${lastNameFinal}`;
+    //     // console.log(fullNameFinal);
+    //   }
+    // }
 
-    showFullname();
+    // showFullname();
 
-    newStudent.firstName = firstNameFinal;
-    newStudent.middleName = middleNameFinal;
-    newStudent.lastName = lastNameFinal;
+    student.firstName = firstNameFinal;
+    student.middleName = middleNameFinal;
+    student.lastName = lastNameFinal;
+    // student.gender = jsonObject.gender
     // newStudent.nickName = nickNameFinal
 
     //newStudent.imageFilename = ;
-    newStudent.house = student.house;
+    student.house = houseFinal;
 
-    allStudents.push(newStudent);
-
-    const split = new Set([firstNameFinal, middleNameFinal, lastNameFinal]);
-    let nameSplit = Array.from(split);
-
-    return split;
+    //store new object with cleaned data
+    allStudents.push(student);
   });
-
+  console.log(allStudents);
   displayList(allStudents);
+  // buildList();
 }
 
-function displayList(students) {
-  console.log("newstudentlist");
-  // clear the list
-  document.querySelector("#list tbody").innerHTML = "";
-
-  // build a new list
-  students.forEach(displayStudent);
-}
-
-function displayStudent(newStudent) {
-  // create clone
-  console.log("displaystud");
-  const firstLetter = newStudent.firstName.substring(0, 1);
-  const clone = document
-    .querySelector("template#student")
-    .content.cloneNode(true);
-
-  // set clone data
-  // clone.querySelector("[data-field=fullName]").textContent = newStudent.fullName;
-  clone.querySelector("[data-field=firstName]").textContent =
-    newStudent.firstName;
-
-  if (newStudent.middleName.includes('"')) {
-    // let nickNameFinal = middleNameFinal
-    // console.log(nickNameFinal)
-    clone.querySelector("[data-field=nickName]").textContent =
-      newStudent.middleName;
-    clone.querySelector("[data-field=middleName]").textContent = " ";
-  } else {
-    clone.querySelector("[data-field=middleName]").textContent =
-      newStudent.middleName;
-    clone.querySelector("[data-field=nickName]").textContent = " ";
-  }
-
-  clone.querySelector("[data-field=lastName]").textContent =
-    newStudent.lastName;
-  // clone.querySelector("[data-field=nickname]").textContent = newStudent.nickName;
-  clone.querySelector("[data-field=avatar]").children[0].src =
-    "images/" + newStudent.lastName + "_" + firstLetter + ".png";
-  clone.querySelector("[data-field=house]").textContent = newStudent.house;
-
-  // append clone to list
-  document.querySelector("#list tbody").appendChild(clone);
-}
-
-//filter
+//----------------filtering
 function selectFilter(event) {
   const filter = event.target.dataset.filter;
-  console.log(`User selected ${filter}`);
+  // console.log(`User selected ${filter}`);
   setFilter(filter);
 }
 
@@ -219,27 +167,21 @@ function isSly(student) {
   }
 }
 
-function buildList() {
-  console.log("buildlist");
-  const currentList = filterList(allStudents);
-  const sortedList = sortList(currentList);
-
-  displayList(sortedList);
-}
-
+//----------sorting
 //generic sort function
+
 function selectSort(event) {
-  console.log("selectsort");
+  // console.log("selectsort");
   const sortBy = event.target.dataset.sort;
   const sortDir = event.target.dataset.sortDirection;
 
   //find "old" sortby element and remove .sortBy
-  const oldElement = document.querySelector(`[data-sort='${settings.sortBy}']`);
-  oldElement.classList.remove("sortby");
-  console.log(oldElement);
+  // const oldElement = document.querySelector(`[data-sort='${settings.sortBy}']`);
+  // oldElement.classList.remove("sortby");
+  // console.log(oldElement);
 
   //indicate active sort
-  event.target.classList.add("sortby");
+  // event.target.classList.add("sortby");
 
   //toggle direction
   if (sortDir === "asc") {
@@ -249,7 +191,6 @@ function selectSort(event) {
   }
 
   console.log(`User selected ${sortBy} - ${sortDir}`);
-
   setSort(sortBy, sortDir);
 }
 
@@ -261,7 +202,7 @@ function setSort(sortBy, sortDir) {
 
 function sortList(sortedList) {
   let direction = 1;
-  if (settings.sortDir === "asc") {
+  if (settings.sortDir === "desc") {
     direction = -1;
   } else {
     settings.direction = 1;
@@ -272,6 +213,7 @@ function sortList(sortedList) {
   function sortByProperty(nameA, nameB) {
     console.log(`sortBy is ${settings.sortBy}`);
     if (nameA[settings.sortBy] < nameB[settings.sortBy]) {
+      console.log(nameA);
       return -1 * direction;
     } else {
       return 1 * direction;
@@ -279,4 +221,55 @@ function sortList(sortedList) {
   }
 
   return sortedList;
+  // console.log(sortedList);
+}
+
+//---------display
+function buildList() {
+  // console.log("buildlist runs");
+  const currentList = filterList(allStudents);
+  const sortedList = sortList(currentList);
+
+  displayList(currentList);
+  // displayList(sortedList);
+}
+
+function displayList(allStudents) {
+  // console.log("displayList runs");
+  // console.log(allStudents);
+  // clear the list
+  document.querySelector("#list tbody").innerHTML = "";
+
+  // build a new list
+  allStudents.forEach(displayStudent);
+}
+
+function displayStudent(student) {
+  // create clone
+  // console.log("displaystud runs");
+  const firstLetter = student.firstName.substring(0, 1);
+  const clone = document.querySelector("template#student").content.cloneNode(true);
+
+  // set clone data
+  // clone.querySelector("[data-field=fullName]").textContent = newStudent.fullName;
+  clone.querySelector("[data-field=firstName]").textContent = student.firstName;
+
+  if (student.middleName.includes('"')) {
+    // let nickNameFinal = middleNameFinal
+    // console.log(nickNameFinal)
+    clone.querySelector("[data-field=nickName]").textContent = student.middleName;
+    clone.querySelector("[data-field=middleName]").textContent = " ";
+  } else {
+    clone.querySelector("[data-field=middleName]").textContent = student.middleName;
+    clone.querySelector("[data-field=nickName]").textContent = " ";
+  }
+
+  clone.querySelector("[data-field=lastName]").textContent = student.lastName;
+  // clone.querySelector("[data-field=nickname]").textContent = newStudent.nickName;
+  clone.querySelector("[data-field=avatar]").children[0].src =
+    "images/" + student.lastName + "_" + firstLetter + ".png";
+  clone.querySelector("[data-field=house]").textContent = student.house;
+
+  // append clone to list
+  document.querySelector("#list tbody").appendChild(clone);
 }
